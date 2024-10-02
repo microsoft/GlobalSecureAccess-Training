321e37e1f0d8b6901284a4dc3259db88354713c5
Andres Canello <39328890+andres-canello@users.noreply.github.com>
2024-10-02 15:43:13 +1000

    Disabled Edit this page link

--- website/docs/KnownIssues.md
+++ website/docs/KnownIssues.md
@@ -1,5 +1,5 @@
 ---
-sidebar_position: 201
+sidebar_position: 700
 title:  Known Issues
 ---
 
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -52,8 +52,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/microsoft/GlobalSecureAccess/blob/main/src/',
+          //editUrl:
+          //  'https://github.com/microsoft/GlobalSecureAccess/blob/main/src/',
         },
         blog: false,
         theme: {
